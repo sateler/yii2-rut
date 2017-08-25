340266da76da1e6d4105db74005c16e2226ad20c
Felipe Sateler <fsateler@gmail.com>
2017-08-25 15:05:01 -0300

    Formatter: return empty string when empty string is passed
    This allows selecting if nullDisplay is used or not

--- RutFormatBehavior.php
+++ RutFormatBehavior.php
@@ -12,15 +12,18 @@ class RutFormatBehavior extends \yii\base\Behavior {
      */
     public $digitSeparator = '.';
 
-	public function asRut($val) {
-        if (!$val) {
+    public function asRut($val) {
+        if ($val === null) {
             return $this->owner->nullDisplay;
+        }
+        if ($val === '') {
+            return $val;
         }
         $val = RutValidator::trimValue($val);
         $sep = strlen($val) - 1;
         $number = substr($val, 0, $sep);
         $verifier = substr($val, $sep);
-        
+
         return number_format($number, 0, ',', $this->digitSeparator) . '-' . strtolower($verifier);
-	}
+    }
 }
